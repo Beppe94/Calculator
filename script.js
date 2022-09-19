abbcda945c70f822b9f12cdd534324f228c25923
Beppe94 <giuseppe.monorchio94@hotmail.it>
2022-09-19 20:32:35 +0200

    added function to clear screen and fixed css positions

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 const grid = document.getElementById('grid')
+const screen = document.getElementById('screen')
+const clearBtn = document.getElementById('clear')
+const prevDisplay = document.getElementById('prevDisplay')
+const currDisplay = document.getElementById('currDisplay')
+const buttons = document.querySelectorAll('.grid button')
+
+console.log(buttons)
+clearBtn.addEventListener('click', clear) 
 
 
+function clear() {
+    currDisplay.textContent = '0'
+    prevDisplay.textContent = ''
+}
